4437e5a61461441b816b1aac7c54621cbb973469
Adrian Roger <adrianroge@gmail.com>
2024-04-12 18:01:42 -0300

    [feat]: aplicação de teste para o servidor grupo 8(orderUp)

---
+++ src/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
