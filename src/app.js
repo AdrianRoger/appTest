1cdd6cd256d8e7dc429bc807250b014b923727b3
Adrian Roger <adrianroge@gmail.com>
2024-04-12 19:00:28 -0300

    [fix]: jsadfvjaksdfv

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(PORT, () => {
